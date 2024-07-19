d89847d1a983935e24ccb7afebe1a0fca45c7d57
denisbelenko <denisdel123@>
2024-07-19 18:49:11 +0700

    добавил шаблон для секретных фраз

--- app/settings.py
+++ app/settings.py
@@ -81,6 +81,8 @@ DATABASES = {
         'NAME': os.environ.get("POSTGRES_DB"),
         'USER': os.environ.get("POSTGRES_USER"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'PORT': os.environ.get("POSTGRES_PORT"),
+        'HOST': os.environ.get("POSTGRES_HOST")
     }
 }
 
